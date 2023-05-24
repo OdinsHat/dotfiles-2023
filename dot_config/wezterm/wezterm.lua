febffffd7428e3e5662f647ba27026bff283a20b
Doug Bromley <doug.bromley@gmail.com>
2023-05-24 03:45:22 +0100

    Initial commit of basics

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,96 @@
+local wezterm = require("wezterm")
+
+local config = {
+    check_for_updates = false,
+    color_scheme = "Builtin Solarized Dark",
+    inactive_pane_hsb = {
+        hue = 1.0,
+        saturation = 1.0,
+        brightness = 1.0,
+    },
+    default_prog = { '/bin/zsh', '-l' },
+    font_size = 11.0,
+    launch_menu = {},
+    leader = { key="a", mods="CTRL" },
+    disable_default_key_bindings = true,
+    default_cursor_style = 'BlinkingBar',
+    keys = {
+        -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+        { key = "a", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x01"}},
+        { key = "-", mods = "LEADER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+        { key = "\\",mods = "LEADER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+        { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
+        { key = "c", mods = "LEADER",       action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+        { key = "h", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
+        { key = "j", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
+        { key = "k", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
+        { key = "l", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
+        { key = "H", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
+        { key = "J", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
+        { key = "K", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
+        { key = "L", mods = "LEADER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 7}}},
+        { key = "1", mods = "LEADER",       action=wezterm.action{ActivateTab=0}},
+        { key = "2", mods = "LEADER",       action=wezterm.action{ActivateTab=1}},
+        { key = "3", mods = "LEADER",       action=wezterm.action{ActivateTab=2}},
+        { key = "4", mods = "LEADER",       action=wezterm.action{ActivateTab=3}},
+        { key = "5", mods = "LEADER",       action=wezterm.action{ActivateTab=4}},
+        { key = "6", mods = "LEADER",       action=wezterm.action{ActivateTab=5}},
+        { key = "7", mods = "LEADER",       action=wezterm.action{ActivateTab=6}},
+        { key = "8", mods = "LEADER",       action=wezterm.action{ActivateTab=7}},
+        { key = "9", mods = "LEADER",       action=wezterm.action{ActivateTab=8}},
+        { key = "&", mods = "LEADER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
+        { key = "x", mods = "LEADER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
+
+        { key = "n", mods="SHIFT|CTRL",     action="ToggleFullScreen" },
+
+	-- Pasting
+        { key = "v", mods="SHIFT|CTRL",     action=wezterm.action.PasteFrom "Clipboard" },
+        { key = "Insert", mods="CTRL",     action=wezterm.action.PasteFrom "Clipboard" },
+	-- Scrolling
+        { key = "PageDown", mods="SHIFT|CTRL",     action=wezterm.action.ScrollByPage(0.5) },
+        { key = "PageUp", mods="SHIFT|CTRL",     action=wezterm.action.ScrollByPage(-0.5) },
+        -- Tab Switching
+	{ key = "PageUp", mods="SHIFT|CTRL",     action=wezterm.action.ActivateTabRelative(1) },
+        { key = "PageDown", mods="SHIFT|CTRL",     action=wezterm.action.ActivateTabRelative(-1) },
+    },
+    set_environment_variables = {},
+}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    config.front_end = "Software" -- OpenGL doesn't work quite well with RDP.
+    config.term = "" -- Set to empty so FZF works on windows
+    config.default_prog = { "cmd.exe" }
+    table.insert(config.launch_menu, { label = "PowerShell", args = {"powershell.exe", "-NoLogo"} })
+
+    -- Find installed visual studio version(s) and add their compilation
+    -- environment command prompts to the menu
+    for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
+        local year = vsvers:gsub("Microsoft Visual Studio/", "")
+        table.insert(config.launch_menu, {
+            label = "x64 Native Tools VS " .. year,
+            args = {"cmd.exe", "/k", "C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat"},
+        })
+    end
+else
+    table.insert(config.launch_menu, { label = "bash", args = {"bash", "-l"} })
+    table.insert(config.launch_menu, { label = "fish", args = {"fish", "-l"} })
+    table.insert(config.launch_menu, { label = "zsh", args = {"zsh", "-l"} })
+    table.insert(config.launch_menu, { label = "xonsh", args = {"xonsh", "-l"} })
+end
+
+wezterm.on('update-right-status', function(window, pane)
+  -- "Wed Mar 3 08:14"
+  local date = wezterm.strftime '%a %b %-d %H:%M '
+
+  local bat = ''
+  for _, b in ipairs(wezterm.battery_info()) do
+    bat = '🔋 ' .. string.format('%.0f%%', b.state_of_charge * 100)
+  end
+
+  window:set_right_status(wezterm.format {
+    { Text = bat .. '   ' .. date },
+  })
+end)
+
+
+return config
